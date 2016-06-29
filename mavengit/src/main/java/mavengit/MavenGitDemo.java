4c622c6230aeac4377540fe46be8bb5ecd4297e4
jeetenderbatra <jeetenderbatra@users.noreply.github.com>
2016-06-29 23:15:05 +0530

    Update MavenGitDemo.java

--- mavengit/src/main/java/mavengit/MavenGitDemo.java
+++ mavengit/src/main/java/mavengit/MavenGitDemo.java
@@ -5,6 +5,7 @@ public class MavenGitDemo {
 	public static void main(String[] args) {
 		System.out.println("Maven Git demo");
 		System.out.println("This is new Version");
+		System.out.println("This is repo change");
 	}
 
 }
